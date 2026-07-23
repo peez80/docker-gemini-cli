00180960d8831088bc4de14a4b17cef33ecc9a5c
peez <peez@stiffi.de>
2026-07-23 11:34:41 +0200

    upate test start script

--- agy.sh
+++ agy.sh
@@ -2,4 +2,5 @@
 docker run -it --privileged \
     -v ${HOME}/.gemini/antigravity-cli:/root/.gemini/antigravity-cli \
     -v ${HOME}/testfiles:/apps/testfiles \
+    --privileged \
     peez/antigravity-cli:latest "$@"
