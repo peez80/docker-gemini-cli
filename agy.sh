22e5b946551a168d6b668c865cf6db3df6393cac
peez <peez@stiffi.de>
2026-08-06 20:40:44 +0200

    update agy.sh to mount the dind docker files, in order to maintain docker artifacts throughout agy runs

--- agy.sh
+++ agy.sh
@@ -2,5 +2,6 @@
 docker run -it \
     -v ${HOME}/.gemini/antigravity-cli:/root/.gemini/antigravity-cli \
     -v $(pwd):/apps \
+    -v antigravity-cli-dind-data:/var/lib/docker \
     --privileged \
     peez/antigravity-cli:latest "$@"
